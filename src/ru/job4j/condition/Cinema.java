c7800194bccbd7d4fe3a1840f60d2621e2c7f187
Elena Kurbanniyazova <TumaevaElena92@yandex.ru>
2020-08-19 17:23:22 +0400

    4.3.1. Оператор && [#337725]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 public class Cinema {
+/*
     public static void access (int age) {
         System.out.println("The age of the customer is: " + age);
         if (age > 16) {
@@ -12,5 +13,20 @@ public class Cinema {
     public static void main(String [] args) {
         access(21);
         access(16);
+    } */
+    @SuppressWarnings("checkstyle:MethodParamPad")
+    public static void permission (boolean allowByParent, boolean hasMoney) {
+    if (allowByParent && hasMoney) {
+        System.out.println("Я могу пойти в кино");
+    } else {
+        System.out.println("Я не могу пойти");
+    }
+    }
+    public static void main(String[] args) {
+        Cinema.permission(true, true);
+        Cinema.permission(true, false);
+        Cinema.permission(false, true);
+        Cinema.permission(false, false);
     }
 }
+
