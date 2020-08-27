c626c7ca929556000a38567304c05efedf020687
Elena-Milena <TumaevaElena92@yandex.ru>
2020-08-27 22:09:27 +0400

    5.2. Создать программу, вычисляющую факториал. [#337833] 3 вариант

--- src/ru/job4j/loop/Factorial.java
+++ src/ru/job4j/loop/Factorial.java
@@ -4,10 +4,9 @@ public class Factorial {
     public static int calc (int n) {
         int result = 1;
         for (int x = 1; x <= n; x++) {
-            if (result == 0) {
-            }
             result = result * x;
         }
         return result;
     }
-}
+    }
+
