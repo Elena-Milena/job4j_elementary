73ad1f66aba3a35a3175407d5dda56c3c1e8d0e7
Elena-Milena <TumaevaElena92@yandex.ru>
2020-08-29 16:57:17 +0400

    5.5. Простое число [#337827]

---
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class CheckPrimeNumberTest extends TestCase {
+    @Test
+public void testWhen5() {
+        boolean rsi = CheckPrimeNumber.chek(5);
+        assertThat(rsi, is(true));
+    }
+    @Test
+    public void testWhen4() {
+        boolean rsl = CheckPrimeNumber.chek(4);
+        assertThat(rsl, is(false));
+    }
+    @Test
+    public void testWhen8() {
+        boolean rsl = CheckPrimeNumber.chek(3);
+        assertThat(rsl, is(true));
+    }
+
+}
