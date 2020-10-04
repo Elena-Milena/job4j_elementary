d2767214721e8a8eed807aaf04f2d1dbf7ece25b
Elena-Milena <TumaevaElena92@yandex.ru>
2020-10-04 19:44:55 +0400

    6.8. Дефрагментация массива [#337764]

---
+++ test/ru/job4j/array/DefragmentTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.array;
+
+import junit.framework.TestCase;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class DefragmentTest extends TestCase {
+
+    public void testSingleFirstNull() {
+        String[] input = {null, "I"};
+        String[] compressed = Defragment.compress(input);
+        String[] expected = {"I", null};
+        assertThat(compressed, is(expected));
+    }
+
+    public void testNotFirstNull() {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = Defragment.compress(input);
+        String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+        assertThat(compressed, is(expected));
+    }
+
+    public void testFirstNull() {
+        String[] input = {null, "I", "wanna", null, "be", null, "compressed"};
+        String[] compressed = Defragment.compress(input);
+        String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+        assertThat(compressed, is(expected));
+    }
+
+    public void testNotAllNull() {
+        String[] input = {null, null, null};
+        String[] compressed = Defragment.compress(input);
+        String[] expected = {null, null, null};
+        assertThat(compressed, is(expected));
+    }
+
+    public void testNotAllAtFirstNull() {
+        String[] input = {null, null, null, "I", "wanna", "be", "compressed"};
+        String[] compressed = Defragment.compress(input);
+        String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+        assertThat(compressed, is(expected));
+    }
+}
